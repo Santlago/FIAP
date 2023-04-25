0d21a72875104201accdc8358ab1378431f4f981
Breno Santiago <83508587+Santlago@users.noreply.github.com>
2023-04-25 13:17:12 +0000

    a

--- CTP/exerc-lacos/exerc-lacos2.py
+++ CTP/exerc-lacos/exerc-lacos2.py
@@ -5,13 +5,46 @@
 # for i in range (n, n * 11, n):
 #     print(i)
 
+
 #-----------------------Exercício 1-V2-------------------
-n = int(input("Digite um número: "))
-p = int(input("Digite um número: "))
+# n = int(input("Digite um número: "))
 
-i = 0
-v = 1
+# i = 0
+# v = 1
 
-for i in range (n, p, n):
-    print(n, "*", v, "=", i)
-    v += 1
+# for i in range (n, n * 11, n):
+#     print(n, "*", v, "=", i)
+#     v += 1
+
+
+#-----------------------Exercício 1-V3-------------------
+# n = int(input("Digite um número: "))
+# p = int(input("Digite um número: "))
+
+# i = 0
+# v = 1
+
+# for i in range (n, p * (n + 1), n):
+#     print(n, "*", v, "=", i)
+#     v += 1
+
+
+#-----------------------Exercício 2-V1-------------------
+# n = 1
+# soma = 0
+
+# while n != 0:
+#     n = int(input("Digite um número: "))
+#     soma += n
+    
+# print(soma)
+
+#-----------------------Exercício 2-V2-------------------
+# n = 1
+# soma = 0
+
+# while n > 0:
+#     n = int(input("Digite um número: "))
+#     soma += n
+    
+# print(soma)
