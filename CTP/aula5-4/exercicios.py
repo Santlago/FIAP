900b0caaef322c5b47dbf36c429b5b99059dfedf
Breno Santiago <83508587+Santlago@users.noreply.github.com>
2023-05-05 13:27:14 +0000

    finalização exercicio primo

--- CTP/aula5-4/exercicios.py
+++ CTP/aula5-4/exercicios.py
@@ -15,20 +15,23 @@
 # print(verificaPrimo(1))
 
 #-----------------Exercício 2----------------
-def mostrarPrimo():
-    n1 = int(input("Digite um número: "))
-    n2 = int(input("Digite um número: "))
+# def verificaPrimo(n: float) -> bool:
+    
+#     if n < 2:
+#         return False
+    
+#     i = 0
+#     for i in range(2, n):
+#         if n % i == 0:
+#             return False
+    
+#     return True
 
-    prime_numbers = 0
-    i = 0
-    
-    for i in range(n1, n2, 1):
-        v = 0
-        for v in range(2, i):
-            if i % v == 0:
-                break
-            else:
-                prime_numbers += 1
-    print(prime_numbers)
-    
-mostrarPrimo()
+# n1 = int(input("Digite um número: "))
+# n2 = int(input("Digite um número: "))
+
+# v = 0
+
+# for v in range(n1, n2 + 1):
+#     if verificaPrimo(v):
+#         print(v)
