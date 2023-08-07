a77ee757368e013b57036b533c455423bf725719
Breno Santiago <83508587+Santlago@users.noreply.github.com>
2023-08-07 11:53:12 +0000

    att

--- RWD/aula2-8/exercicio/js/script.js
+++ RWD/aula2-8/exercicio/js/script.js
@@ -1,14 +1,23 @@
-let btn = document.getElementById('botao')
-let lista = document.getElementById('lista')
-let tarefa = document.getElementById('valor')
-
 let list = []
+let btn = document.querySelector('button')
+let lista = document.querySelector('ul')
+let tarefa = document.getElementById('#idTarefa')
 
-btn.addEventListener('click', () => {
-    list.push(valor.value)
-    for (let i = 0; i < list.length; i++) {
-        list.splice(i, 1, `<li>${list[i]}</li>`)
-    }
-    lista.innerHTML = list
-    
+
+btn.addEventListener('click', function(){
+    list.push(tarefa.value)
+    console.log(list);
+    lista.innerHTML = ''
+    list.forEach((item, i)=>{
+        lista.innerHTML += `<li>${item} <button onclick="apagar('${i}')"> X </button></li>`
+    })
+    tarefa.value = ''
 });
+
+function apagar(i){
+    list.splice(i, 1)
+    lista.innerHTML = ''
+    list.forEach((item, i)=>{
+        lista.innerHTML += `<li>${item} <button onclick="apagar('${i}')"> X </button></li>`
+    })
+}
