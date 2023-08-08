df9156ddcc0f1e0e1e4dd72b8c3bc9003b7225a8
Breno Santiago <83508587+Santlago@users.noreply.github.com>
2023-08-08 11:31:01 +0000

    att

---
+++ DDD/aula8-8/PrimeiroArray/src/Teste.java
@@ -0,0 +1,22 @@
+public class Teste {
+    public static void main(String[] args) {
+        
+        // int idadeUm= 37;
+        // int idadeDois = 18;
+        // int idadeTres = 21;
+
+        int idade [] = new int [3];
+
+        idade [0] = 37;
+        idade [1] = 18;
+        idade [2] = 21;
+
+        // System.out.println(idade [0]);
+        // System.out.println(idade [1]);
+        // System.out.println(idade [2]);
+
+        for(int i=0; i<3; i++) {
+            System.out.println(idade[i]);
+        }
+    }
+}
