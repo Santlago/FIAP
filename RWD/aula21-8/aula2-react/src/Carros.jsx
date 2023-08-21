33b00d94df8c2dd2f0cfca3f910d6a03154d8e4c
Santlago <breno.santiago.8@gmail.com>
2023-08-21 09:38:38 -0300

    finalizando aula 21-8

---
+++ RWD/aula21-8/aula2-react/src/Carros.jsx
@@ -0,0 +1,14 @@
+function Carros() {
+const imgStyle = {
+    height: '200px',
+    width: '200px',
+};
+
+    return (
+        <div>
+            <img src="https://preview.redd.it/o1l2x13dbl741.jpg?auto=webp&s=f08ce2ce7bff841322de8d35e8eb4a55b55d12e0" alt="rx-7" style={imgStyle}/>
+        </div>
+    )
+}
+
+export default Carros
---
+++ RWD/aula21-8/aula2-react/src/Parceiros.jsx
@@ -0,0 +1,13 @@
+function Parceiros(params) {
+    return (
+        <div>
+            <h2>Parceiros</h2>
+            <a href="http://facebook.com">Facebook</a><br />
+            <a href="http://amazon.com">Amazon</a><br />
+            <a href="http://twitch.com">Twitch</a><br />
+            <a href="http://youtube.com">YouTube</a><br />
+        </div>
+    )
+}
+
+export default Parceiros
