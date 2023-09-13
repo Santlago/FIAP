fce33188eb07ef41434af8dec44ec0589a661b1d
Santlago <breno.santiago.8@gmail.com>
2023-09-13 10:45:28 -0300

    testequedeuerrad

--- RWD/cp2-2/src/routes/Home/index.jsx
+++ RWD/cp2-2/src/routes/Home/index.jsx
@@ -1,8 +1,13 @@
+import Aparelho1 from "../../../public/aparelho1.jpg"
+import Aparelho2 from "../../../public/aparelho2.jpg"
+
 export default function Home() {
     
     return(
         <main>
             <h1>Home</h1>
+            <img src={Aparelho1} alt="aparelho 1"/>
+            <img src={Aparelho2} alt="aparelho 2"/>
         </main>
     )
 }
