098f88568aeeef1d97782fbb9007823d1ee2bbe2
Santlago <breno.santiago.8@gmail.com>
2023-09-18 22:13:35 -0300

    att

--- RWD/cp2-2/src/App.jsx
+++ RWD/cp2-2/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import Cabecalho from './components/Cabecalho'
 import Rodape from './components/Rodape'
-import Menu from './components/Menu'
 
 function App() {
   return (
     <>
       <Cabecalho/>
-      <Menu/>
       <Rodape/>
     </>
   )
