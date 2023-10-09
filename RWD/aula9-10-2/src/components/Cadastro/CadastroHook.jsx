cdcfa0be327d9663a445235d82a4fce30fa73e38
Santlago <breno.santiago.8@gmail.com>
2023-10-09 17:24:31 -0300

    att

--- RWD/aula9-10-2/src/components/Cadastro/CadastroHook.jsx
+++ RWD/aula9-10-2/src/components/Cadastro/CadastroHook.jsx
@@ -23,28 +23,79 @@ export const CadastroHook = () => {
 
     const [listaClientes, setListaClientes] = useState([])
 
+    const buscarCep = (e) => {
+        const cep = e.target.value.replace(/\D/g, '')
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(resp => resp.json())
+        .then(data => {
+            setValue('rua', data.logradouro)
+            setValue('bairro', data.bairro)
+            setValue('cidade', data.localidade)
+            setValue('estado', data.uf)
+            setFocus('numero')
+        })
+    }
+
     const inserirCliente = (cliente) => {
-        setListaClientes([listaClientes, cliente])
+        setListaClientes([...listaClientes, cliente])
     }
 
     return(
-        <div>
+        <div className='divCadastro'>
             <h1>CadastroHook</h1>
             <form onSubmit={handleSubmit(inserirCliente)}>
                 <fieldset>
                     <legend>Dados Pessoais</legend>
                     <label>Nome:
                         <input type="text" {...register("nome")}/>
+                        <span>{errors.nome?.message}</span>
                     </label>
                     <label>E-mail:
                         <input type="text" {...register("email")}/>
+                        <span>{errors.email?.message}</span>
                     </label>
                     <label>CPF:
                         <input type="text" {...register("cpf")}/>
+                        <span>{errors.cpf?.message}</span>
                     </label>
                     <button type='submit'>Cadastrar</button>
                 </fieldset>
+                <fieldset>
+                    <legend>Endereço</legend>
+                    <label>CEP:
+                        <input type="text" {...register('cep')} onBlur={buscarCep}/>
+                    </label>
+                    <label>Rua:
+                        <input type="text" {...register('rua')}/>
+                    </label>
+                    <label>Número:
+                        <input type="text" {...register('numero')}/>
+                    </label>
+                    <label>Bairro:
+                        <input type="text" {...register('bairro')}/>
+                    </label>
+                    <label>Cidade:
+                        <input type="text" {...register('cidade')}/>
+                    </label>
+                    <label>Estado:
+                        <input type="text" {...register('estado')}/>
+                    </label>
+                </fieldset>
             </form>
+            <div className="painel">
+                {
+                    listaClientes.map((cli, i) => 
+                        <div key={i} className='etiqueta'>
+                            <p>Nome: {cli.nome}</p>
+                            <p>E-mail: {cli.email}</p>
+                            <p>CPF: {cli.cpf}</p>
+                            <p>{cli.rua}, {cli.numero}</p>
+                            <p>Bairro: {cli.bairro}</p>
+                            <p>Cidade: {cli.cidade} - {cli.estado}</p> 
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
