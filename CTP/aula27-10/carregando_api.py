b54d063710f3840963d45d8d96c1831ef18d085c
Santlago <breno.santiago.8@gmail.com>
2023-10-27 11:10:27 -0300

    att

--- CTP/aula27-10/carregando_api.py
+++ CTP/aula27-10/carregando_api.py
@@ -1,22 +1,30 @@
 import requests
+from tkinter import Tk, Label, Button
 
 def pegar_cotacoes():
     # Direcionei a requisição da API e forneci os dados que eu preciso
     requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
     requisicao_dic = requisicao.json()
 
-    print(requisicao_dic)
-
     cotacao_dolar = requisicao_dic['USDBRL']['bid']
     cotacao_euro = requisicao_dic['EURBRL']['bid']
     cotacao_btc = requisicao_dic['BTCBRL']['bid']
 
-    texto = f'''
+    texto_resposta['text'] = f'''
     Dólar: {cotacao_dolar}
     Euro: {cotacao_euro}
     Bitcoin: {cotacao_btc}
     '''
 
-    print(texto)
+janela = Tk()
+janela.title('Cotações')
 
-pegar_cotacoes()
+texto = Label(janela, text = 'Clique no botão para ver as cotações')
+texto.grid(column=0, row=0, padx=10, pady=10)
+
+botao = Button(janela, text="Buscar", command=pegar_cotacoes)
+botao.grid(column=0, row=1, padx=10, pady=10)
+
+texto_resposta = Label(janela, text='')
+texto_resposta.grid(column=0, row=2, padx=0, pady=0)
+janela.mainloop()
