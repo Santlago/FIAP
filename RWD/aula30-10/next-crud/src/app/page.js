3079633e6d50150f0d14bcfdd24360000be376ee
Santlago <breno.santiago.8@gmail.com>
2023-10-30 09:37:42 -0300

    att

--- RWD/aula30-10/next-crud/src/app/page.js
+++ RWD/aula30-10/next-crud/src/app/page.js
@@ -1,3 +1,5 @@
+"use client" // client component - Ele tem conteúdo dinâmico
+
 import { useEffect, useState } from "react"
 
 export default function Home() {
@@ -13,9 +15,48 @@ export default function Home() {
     }, []
   )
 
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/produto/${id}`, {
+      method: 'delete'
+    })
+    .then(window.location = '/')
+    .catch(error => console.error(error))
+  }
+
   return (
     <main>
-      <h1>Aula de CRUD</h1>
+      <h1>Lista de Produtos</h1>
+
+
+    <table>
+      <thead>
+        <tr>
+          <th>Título</th>
+          <th>Quant.</th>
+          <th>Preço</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          produtos.map(prod => (
+            <tr key={prod.id}>
+              <td>{prod.titulo}</td>
+              <td>{prod.quantidade}</td>
+              <td>R${parseFloat(prod.preco).toFixed(2)}</td>
+              <td>
+                <button onClick={handleDelete.bind(this, prod.id)}>Excluir</button>
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan={4}>Produtos vindos do Servidor</td>
+        </tr>
+      </tfoot>
+    </table>
     </main>
   )
 }
