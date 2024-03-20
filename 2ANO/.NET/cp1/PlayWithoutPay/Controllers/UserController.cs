f3cb21f351bbf6b30d7ea0ad8222e8615adacbd3
Santlago <breno.santiago.8@gmail.com>
2024-03-20 09:33:42 -0300

    feat: adding cp1pt2 file

--- 2ANO/.NET/cp1/PlayWithoutPay/Controllers/UserController.cs
+++ 2ANO/.NET/cp1/PlayWithoutPay/Controllers/UserController.cs
@@ -23,14 +23,14 @@ namespace PlayWithoutPay.Controllers
         [HttpPost]
         public IActionResult Login(User user)
         {
-            User userLogin = null; // Initialize userLogin outside of the if block
+            User userLogin = null; 
 
             if (user != null)
             {
                 userLogin = new User(user.Login, user.Password, user.Id);
             }
 
-            return View("~/Views/Home/Index.cshtml", userLogin); // Assuming "User/Index" is the correct path to the view
+            return View("~/Views/Home/Index.cshtml", userLogin); 
         }
     }
 }
--- 2ANO/IOT/ex1cp1/README.md
+++
@@ -1,8 +0,0 @@
-# Exercício para o CheckPoint 1
-Usar tecnicas de classificação em um conjunto de dados de sua escolha
-
-- Gráficos de dispersão
-- Padronização (standard scaler) E/OU NORMALIZAÇÃO (MinMaxScaler)
-- DEMONSTRAR ALGUMA TÉCNICA DE TRATAMENTO DE OUTLIERS
-- MÉTRICAS: ACURÁCIA, PRECISÃO E RECALL
-- TREINAR E COMPARAR DOIS MODELOS USANDO DOIS ALGORITMOS DIFERENTES
